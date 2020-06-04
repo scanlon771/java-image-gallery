06d834860e376488136ee6c582e47a75db294d1d
Kenneth Scanlon <kms0156@auburn.edu>
2020-06-04 06:09:50 +0000

    Added S3 utility class

---
+++ src/main/java/edu/au/cc/gallery/s3.java
@@ -0,0 +1,45 @@
+package edu.au.cc.gallery;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+public class s3 {
+   private static final Region  region = Region.US_WEST_2;
+   private S3Client client;
+	
+   public void connect() {
+      Region region = Region.US_WEST_2;
+      client = S3Client.builder().region(region).build();
+   }
+
+   public void createBucket(String bucketName) {
+      CreateBucketRequest createBucketRequest = CreateBucketRequest
+        .builder()
+        .bucket(bucketName)
+        .createBucketConfiguration(CreateBucketConfiguration.builder()
+                .locationConstraint(region.id())
+                .build())
+        .build();
+      client.createBucket(createBucketRequest);
+   }
+
+   public void putObject(String bucketName, String key, String value) {
+     PutObjectRequest por = PutObjectRequest.builder()
+        .bucket(bucketName)
+        .key(key)
+	.build();
+     client.putObject(por, RequestBody.fromString(value));
+   }
+
+   public static void demo() {
+     String bucketName = "edu.au.cc.image-gallery6";
+     s3 s3 = new s3();
+     s3.connect();
+    // s3.createBucket(bucketName);
+     s3.putObject(bucketName, "banana", "yellow");
+   }
+}
